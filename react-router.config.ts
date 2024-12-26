82490b340367ac2c4b5cc39cf16430dfc6abd034
Sergey Plakin <plakinsm@gmail.com>
2024-12-26 13:51:49 +0400

    fix react-router basename

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,6 +4,7 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
+  basename: process.env.GH_PAGES ? '/bingo2025/' : '/',
   async prerender() {
     return ['/'];
   },
