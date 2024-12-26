71f44c5e1a87c5c472d604771ae563031c204d72
Sergey Plakin <plakinsm@gmail.com>
2024-12-26 13:59:43 +0400

    theme color

--- app/root.tsx
+++ app/root.tsx
@@ -45,19 +45,13 @@ export const links: Route.LinksFunction = () => [
   { rel: 'stylesheet', href: stylesheet },
 ];
 
-export const meta: Route.MetaFunction = () => [
-  {
-    name: 'theme-color',
-    content: '#2563eb',
-  },
-];
-
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#2563eb" />
         <Meta />
         <Links />
       </head>
